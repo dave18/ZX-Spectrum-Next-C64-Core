30d0c0c4e0b5dc9d2d6ae391efde6e24300c5d3f
David Douglas <fasterthanasnail@googlemail.com>
2026-09-23 21:36:21 +0100

    Delete C64.srcs/sources_1/new/rtl/CtrlModule/Firmware directory

--- C64.srcs/sources_1/new/rtl/CtrlModule/Firmware/fileselector.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILESELECTOR_H
-#define FILESELECTOR_H
-
-
-void process_files(int raw_code, int joy_code);
-void FileSelector_Init(int filter);
-//void FileSelector_SetLoadFunction(int (*func)(const char *filename));
-
-#endif
