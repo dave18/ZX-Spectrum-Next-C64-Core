e5557e3090c8244d5e52ce34387b4bca5b5388af
David Douglas <fasterthanasnail@googlemail.com>
2026-09-23 21:32:21 +0100

    Delete C64.srcs/sources_1/new/rtl/roms directory

--- C64.srcs/sources_1/new/rtl/roms/as.bat
+++
@@ -1,3 +0,0 @@
-pasmo --hex z80rom.asm filename.hex
-hex2coe
-copy filename.coe z80rom.coe
--- C64.srcs/sources_1/new/rtl/roms/stdrom.bat
+++
@@ -1,4 +0,0 @@
-COPY /b BASIC + KERNAL + dos1541 std_C64.ROM
-bin2hex 0x00 std_C64.ROM filename.hex
-hex2coe
-rename filename.coe std_C64.coe
